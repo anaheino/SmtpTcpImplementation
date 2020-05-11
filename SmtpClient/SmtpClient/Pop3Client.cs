215e4b557d6c2608a7833a6161a75e20bff43bad
anaheino <anaheino@gmail.com>
2020-05-11 17:20:10 +0300

    Implemented simple pop commands in smptclient

---
+++ SmtpClient/SmtpClient/Pop3Client.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace SmtpClient
+{
+    internal class Pop3Client
+    {
+        private string user;
+        private string password;
+        private string mailServer;
+        private TcpClient tcpClient;
+        private SslStream stream;
+        private StreamWriter streamWriter;
+        private StreamReader streamReader;
+
+        public Pop3Client(string user, string password, string mailServer)
+        {
+            this.user = user;
+            this.password = password;
+            this.mailServer = mailServer;
+            tcpClient = new TcpClient();
+        }
+
+        internal string OpenInbox()
+        {
+            string result = "";
+            if (!tcpClient.Connected) return "ERROR! Not connected!";
+            try
+            {
+                Write("LIST" + Environment.NewLine);
+                result = Read(true);
+            }
+            catch (Exception e)
+            {
+
+            }
+            return result;
+        }
+
+        internal void Connect()
+        {
+            tcpClient.Connect(mailServer, 995);
+
+            stream = new SslStream(tcpClient.GetStream());
+            stream.AuthenticateAsClient(mailServer);
+            streamWriter = new StreamWriter(stream);
+            streamReader = new StreamReader(stream);
+        }
+
+        internal string Login()
+        {
+            string resultString = "";
+            try
+            {
+                if (!tcpClient.Connected) return "";
+                resultString = Read();
+                Write("USER " + user.Trim() + Environment.NewLine);
+                resultString = Read();
+                Write("PASS " + password + Environment.NewLine);
+                resultString = Read();
+
+            }
+            catch (Exception e)
+            {
+                return $"Ran into problems while logging in! Latest received message: {resultString} StackTrace : {e.StackTrace}";
+            }
+            return resultString;
+        }
+
+        private void Write(string data)
+        {
+            var byteData = System.Text.Encoding.ASCII.GetBytes(data.ToCharArray());
+            stream.Write(byteData, 0, byteData.Length);
+            stream.Flush();
+        }
+
+        internal string Disconnect()
+        {
+            Write("QUIT" + Environment.NewLine);
+            string result = Read();
+            tcpClient.Dispose();
+            return result;
+        }
+
+        private string Read(bool readToEnd = false)
+        {
+            string resultString = ""; 
+            if (readToEnd)
+            {
+                string line = "";
+                while (!line.Equals("."))
+                {
+                    
+                    line = streamReader.ReadLine();
+                    resultString += line;
+                }
+                return resultString;
+            }
+            resultString = streamReader.ReadLine();
+            return resultString;
+        }
+    }
+}
--- SmtpClient/SmtpClient/Program.cs
+++ SmtpClient/SmtpClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Mail;
+using System.Net.Sockets;
 using System.Threading;
 
 namespace SmtpClient
@@ -8,8 +9,10 @@ namespace SmtpClient
     {
         static void Main(string[] args)
         {
+            /*
             while (true)
             {
+            THIS IS HERE PURELY FOR TESTING SMTP SERVER EASILY
                 System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("localhost");
                 MailMessage msg = new MailMessage("laa@blaa.com", "luu@blaa.com", "hoh", "paskaa");
                 smtp.Send(msg);//Handles all messages in the protocol
@@ -17,6 +20,16 @@ namespace SmtpClient
                 smtp.Dispose();//sends a Quit message    Console.WriteLine("Hello World!");
 
             }
+            */
+            Pop3Client client = new Pop3Client("xxx", "xxxx", "pop.gmail.com");
+            client.Connect();
+            string login = client.Login();
+            if (!login.Contains("OK")) Console.WriteLine($"Ran into problems! Error info: {login}");
+            string listResults = client.OpenInbox();
+            if (!listResults.Contains("OK")) Console.WriteLine($"Ran into problems! Error info: {listResults}");
+            string disconnect = client.Disconnect();
+            if (!disconnect.Contains("OK")) Console.WriteLine($"Ran into problems quitting session! Error info: {disconnect}");
+
         }
     }
 }
