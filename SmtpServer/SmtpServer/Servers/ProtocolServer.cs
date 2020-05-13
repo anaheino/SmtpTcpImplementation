d246c1572534b6bcb129808af5602421994a4555
anaheino <anaheino@gmail.com>
2020-05-13 10:23:27 +0300

    Refactored according to best practices

---
+++ SmtpServer/SmtpServer/Servers/ProtocolServer.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmtpServer.Server
+{
+    public class ProtocolServer
+    {
+        private const int byteSize = 8192;
+
+        internal TcpClient tcpClient { get; set; }
+        internal NetworkStream stream { get; set; }
+
+        internal void Init(TcpClient client)
+        {
+            tcpClient = client;
+            stream = tcpClient.GetStream();
+        }
+
+        internal async Task Write(string strMessage)
+        {
+            var networkStream = tcpClient.GetStream();
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            byte[] buffer = encoder.GetBytes(strMessage + "\r\n");
+            await networkStream.WriteAsync(buffer, 0, buffer.Length);
+            await networkStream.FlushAsync();
+        }
+
+        internal async Task<string> Read()
+        {
+            byte[] messageBytes = new byte[byteSize];
+            int bytesRead = 0;
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            bytesRead = await stream.ReadAsync(messageBytes, 0, byteSize);
+            string strMessage = encoder.GetString(messageBytes, 0, bytesRead);
+            return strMessage;
+        }
+
+    }
+}
---
+++ SmtpServer/SmtpServer/Servers/SMTPServer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmtpServer.Server
+{
+    public class SMTPServer : ProtocolServer, IMailServer
+    {
+        public readonly char[] separators = new char[2] { '<', '>' };
+        private const string mailFrom = "MAIL FROM:";
+        private const string recipient = "RCPT TO:";
+        private const string ehlo = "EHLO";
+        private const string helo = "HELO";
+        // RL this would be an sql db or something to that sort.
+        private bool messageIncoming;
+
+        public SslStream SslStream { get; private set; }
+
+        public async Task Run()
+        {
+            await RunSmtp();
+        }
+
+        private async Task RunSmtp()
+        {
+            string strMessage = String.Empty;
+            Email email = new Email();
+
+            await Write("220 localhost -- Stub Email server");
+
+            while (true)
+            {
+                try
+                {
+                    strMessage = await Read();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exception: {e.StackTrace}");
+                    break;
+                }
+
+                if (strMessage.Length > 0)
+                {
+                    if (strMessage.StartsWith("QUIT"))
+                    {
+                        email.Quit = strMessage;
+                        await Write("221 OK");
+                        tcpClient.Close();
+                        break;
+                    }
+                    else if (strMessage.StartsWith(ehlo) || strMessage.StartsWith(helo))
+                    {
+                        email.Helo = strMessage;
+                        await Write("250 OK");
+                    }
+
+                    else if (strMessage.StartsWith(recipient))
+                    {
+                        email.RecipientRaw = strMessage;
+                        email.Recipient = ParseFromMessage(strMessage, recipient);
+                        await Write("250 OK");
+                    }
+
+                    else if (strMessage.StartsWith(mailFrom))
+                    {
+                        email.SenderRaw = strMessage;
+                        email.Sender = ParseFromMessage(strMessage, mailFrom);
+                        await Write("250 OK");
+                    }
+
+                    else if (strMessage.StartsWith("DATA"))
+                    {
+                        await Write("354 Start mail input; end with");
+                        email.Data = await Read();
+                        messageIncoming = true;
+                        await Write("250 OK");
+                    }
+                    else if (messageIncoming)
+                    {
+                        email.TextPlain = strMessage;
+                        messageIncoming = false;
+                        MailSingleton.emails.Add(email);
+                    }
+                }
+            }
+        }
+
+        private string ParseFromMessage(string strMessage, string msgType)
+        {
+            string sender = strMessage.Remove(0, (msgType + separators[0]).Length);
+            int index = sender.IndexOf(separators[1]);
+            sender = sender.Substring(0, index);
+            return sender;
+        }
+    }
+}
