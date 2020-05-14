72b93ee53c3a8d5941318c51a4b23ae9547dec07
anaheino <anaheino@gmail.com>
2020-05-15 00:51:19 +0300

    added initial stub for FTP implementation

---
+++ FtpClient/FTPClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FTPClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            FtpClient ftpClient = new FtpClient("localhost", 21, 100);
+            await ftpClient.Connect();
+            await ftpClient.Retrieve("C:\\joku.txt");
+            await ftpClient.Disconnect();
+        }
+    }
+}
