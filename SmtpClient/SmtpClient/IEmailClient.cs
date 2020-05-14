364ac21f763b84bfd78c4cbf80288a02bbaf7033
anaheino <anaheino@gmail.com>
2020-05-14 08:53:44 +0300

    Started IMAPclient implementation

---
+++ SmtpClient/SmtpClient/IEmailClient.cs
@@ -0,0 +1,10 @@
+﻿namespace SmtpClient
+{
+    internal interface IEmailClient
+    {
+        void Connect(bool ssl = true);
+        bool Login();
+        string OpenInbox(int index = 0);
+        string Disconnect();
+    }
+}
---
+++ SmtpClient/SmtpClient/IMapClient.cs
@@ -0,0 +1,36 @@
+﻿namespace SmtpClient
+{
+    internal class IMapClient : IEmailClient
+    {
+        private string username;
+        private string password;
+        private string server;
+
+        public IMapClient(string username, string password, string server)
+        {
+            this.username = username;
+            this.password = password;
+            this.server = server;
+        }
+
+        public void Connect(bool ssl = true)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Disconnect()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Login()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string OpenInbox(int index = 0)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
