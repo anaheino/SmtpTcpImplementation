b7bb0cb2a735736aba74c0fb8609b90f0212698c
anaheino <anaheino@gmail.com>
2020-05-17 11:05:05 +0300

    Added working TFTP implementation stub

--- FtpClient/FTPClient/Clients/TftpClient.cs
+++ FtpClient/FTPClient/Clients/TftpClient.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,41 +8,115 @@ namespace FTPClient
 {
     internal class TftpClient
     {
-        private string address;
-        private int port;
-        private UdpClient udpClient;
+        private IPEndPoint serverEndpoint;
+        private EndPoint dataEndpoint;
+        private Socket tftpSocket;
+
+        public enum Opcodes
+        {
+            Unknown = 0,
+            Read = 1,
+            Write = 2,
+            Data = 3,
+            Ack = 4,
+            Error = 5
+        }
+        public enum Modes
+        {
+            Unknown = 0,
+            NetAscii = 1,
+            Octet = 2,
+            Mail = 3
+        }
 
         public TftpClient(string address, int port)
         {
-            this.address = address;
-            this.port = port;
-            udpClient = new UdpClient(address, port);
+            IPHostEntry host = Dns.GetHostEntry(address);
+            serverEndpoint = new IPEndPoint(host.AddressList[0], port);
+            dataEndpoint = serverEndpoint;
+            serverEndpoint.Port = ((IPEndPoint)dataEndpoint).Port;
         }
 
-        internal async Task Get()
+        internal async Task GetTextFile(string filePath)
         {
-            await Write(1, "joku.txt");
-            string msg = await Read();
-        }
+            // Temp solution with partly hardcoded path, in my case I wanted to store everything received to C:/dev/ -folder.
+            BinaryWriter fileStream = new BinaryWriter(new FileStream("C:/dev/" + filePath, FileMode.Create, FileAccess.Write, FileShare.Read));
+            tftpSocket = new Socket(serverEndpoint.Address.AddressFamily, SocketType.Dgram, ProtocolType.Udp)
+            {
+                ReceiveTimeout = 3000
+            };
+            byte[] sendBuffer = CreateRequest(Opcodes.Read, filePath, Modes.Octet);
+            byte[] receiveBuffer = new byte[516];
 
-        private async Task Write(short type, string fileName)
-        {
-            byte[] opCode = BitConverter.GetBytes(type);
-            byte[] filenameBytes = Encoding.ASCII.GetBytes(fileName);
+            tftpSocket.SendTo(sendBuffer, sendBuffer.Length, SocketFlags.None, serverEndpoint);
             
-            List<byte> actualMessage = opCode.ToList();
-            actualMessage.AddRange(filenameBytes.ToList());
-            actualMessage.Add(0);
-            actualMessage.AddRange(Encoding.ASCII.GetBytes("netascii").ToList());
-            actualMessage.Add(0);
-
-            await udpClient.SendAsync(actualMessage.ToArray(), actualMessage.Count);
+            int messageLength = tftpSocket.ReceiveFrom(receiveBuffer, ref dataEndpoint);
+            int packetNumber = 1;
+            bool continueReceiving = true;
+            while (continueReceiving)
+            {
+                if (((Opcodes)receiveBuffer[1]) == Opcodes.Error)
+                {
+                    //If we get here, something was messed up.
+                    fileStream.Close();
+                    tftpSocket.Close();
+                    continueReceiving = false;
+                }
+                // If packet number is correct, this is the thing to save to a file.
+                // Some extra handling would be necessary for longer files, but for excercise purposes, this will do fine.
+                if (receiveBuffer[3] == packetNumber)
+                {
+                    // Writing everything to file, except the headers. So data only. 
+                    // So here we receive the data to be written to the file.
+                    fileStream.Write(receiveBuffer, 4, messageLength - 4);
+                    sendBuffer = CreateAckPacket(packetNumber++);
+                    await tftpSocket.SendToAsync(sendBuffer, SocketFlags.None, serverEndpoint);
+                }
+                if (messageLength < 516)
+                {
+                    // This means it was the final packet
+                    continueReceiving = false;
+                }
+                else
+                {
+                    messageLength = (await tftpSocket.ReceiveFromAsync(receiveBuffer, SocketFlags.None, dataEndpoint)).ReceivedBytes;
+                }
+            }
+            tftpSocket.Close();
+            fileStream.Close();
         }
 
-        private async Task<string> Read()
+        private byte[] CreateAckPacket(int blockNumber)
         {
-            var response = await udpClient.ReceiveAsync();
-            return Encoding.ASCII.GetString(response.Buffer);
+            byte[] ret = new byte[4];
+
+            // Set type as Ack and set return number.
+            ret[0] = 0;
+            ret[1] = (byte)Opcodes.Ack;
+            ret[2] = (byte)((blockNumber >> 8) & 0xff);
+            ret[3] = (byte)(blockNumber & 0xff);
+            return ret;
         }
+
+        private byte[] CreateRequest(Opcodes opCode, string remoteFile, Modes tftpMode)
+        {
+            int index = 0;
+            string mode = tftpMode.ToString().ToLowerInvariant();
+            byte[] ret = new byte[mode.Length + remoteFile.Length + 4];
+            
+            //Setting the first bytes to be req type indentifier
+            ret[index++] = 0;
+            ret[index++] = (byte)opCode;
+            // Setting the index to skip forward the amount of bytes the filename has converted to integer,
+            // effectively storing filename in the buffer.
+            index += Encoding.ASCII.GetBytes(remoteFile, 0, remoteFile.Length, ret, index);
+            // This signals the end of the filename and data section
+            ret[index++] = 0;
+            index += Encoding.ASCII.GetBytes(mode, 0, mode.Length, ret, index);
+            // This signals the end of the mode section
+            ret[index] = 0;
+            return ret;
+        }
+
     }
 }
--- FtpClient/FTPClient/Program.cs
+++ FtpClient/FTPClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace FTPClient
@@ -7,21 +8,30 @@ namespace FTPClient
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //await RunFtp();
-            await RunTftp();
+            var tasks = new List<Task>()
+            {
+                Task.Run(async () =>
+                {
+                    await RunTftp();
+                }),
+                Task.Run(async () =>
+                {
+                    await RunFtp();
+                }),
+            };
+            Task.WaitAll(tasks.ToArray());
         }
 
         private static async Task RunTftp()
         {
-            TftpClient tftpClient = new TftpClient("localhost", 69);
-            await tftpClient.Get();
+            TftpClient tftpClient = new TftpClient("127.0.0.1", 69);
+            await tftpClient.GetTextFile("joku.txt");
         }
 
         private static async Task RunFtp()
         {
             FtpClient ftpClient = new FtpClient("localhost", 21);
-            await ftpClient.Connect();
+            await ftpClient.Connect("hullumies", "hulluheina");
             await ftpClient.SetPassive();
             await ftpClient.ListContents();
             string retrContents = await ftpClient.Retrieve("joku.txt");
